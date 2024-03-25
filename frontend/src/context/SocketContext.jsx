e20713f06200ee3122ad182ec6135a8d9f15cf83
pranavagni1516 <pranav.agni1516@gmail.com>
2024-03-26 02:53:56 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5173/", {
+			const socket = io("https://chattingmessenger.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
